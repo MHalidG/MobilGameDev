9e34452007a7b538225926f633b383659cb52601
Halid <halid.gundz@gmail.com>
2023-05-28 00:45:09 +0200

    GD-C#-V9:AttributesAndReflections

---
+++ CSharpInstructor/Attributes/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer=new Customer { Id=1,LastName="Dagdeviren",Age=32};
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);//method eski uyarisi aliyor
+        }
+    }
+
+    [ToTable("Customers")]
+    [ToTable("TblCustomers")]//Multiple Kullanim
+    //Anotation lara benziyorlar
+    class Customer { 
+    
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string Name { get; set; }
+        [RequiredProperty]
+
+        public string LastName { get; set; }
+        [RequiredProperty]
+
+        public int Age { get; set; }
+
+    }
+
+    class CustomerDal {
+
+        [Obsolete("Add kullanma onun yerine AddNew Kullan")]//Deprecated gibi.
+        public void Add(Customer customer) {
+
+            Console.WriteLine("{0},{1},{2},{3}", customer.Id, customer.Name, customer.LastName, customer.Age);
+        
+        }
+
+        public void AddNew(Customer customer)
+        {
+
+            Console.WriteLine("{0},{1},{2},{3}", customer.Id, customer.Name, customer.LastName, customer.Age);
+
+        }
+
+
+
+    }
+    //[AttributeUsage(AttributeTargets.Class)] //Sadece Classlarda kullan
+    //[AttributeUsage(AttributeTargets.All)] Herseyeuygulanir
+    //[AttributeUsage(AttributeTargets.Method)] Methodlara uygulanir
+    //[AttributeUsage(AttributeTargets.Property|AttributeTargets.Method)]// Propertylere ve Methodlara uygulanir
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple=true)]//sadece Propertylere uygulanir Alt alta kullanilabilir mi
+    class RequiredPropertyAttribute : Attribute { 
+    
+    }
+    [AttributeUsage(AttributeTargets.Class,AllowMultiple = true)] 
+    class ToTableAttribute : Attribute
+    {
+
+        private string _tableName;
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+
+    }
+
+    internal struct NewStruct
+    {
+        public object Item1;
+        public object Item2;
+
+        public NewStruct(object ıtem1, object ıtem2)
+        {
+            Item1 = ıtem1;
+            Item2 = ıtem2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is NewStruct other &&
+                   EqualityComparer<object>.Default.Equals(Item1, other.Item1) &&
+                   EqualityComparer<object>.Default.Equals(Item2, other.Item2);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1030903623;
+            hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(Item1);
+            hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(Item2);
+            return hashCode;
+        }
+
+        public void Deconstruct(out object ıtem1, out object ıtem2)
+        {
+            ıtem1 = Item1;
+            ıtem2 = Item2;
+        }
+
+        public static implicit operator (object, object)(NewStruct value)
+        {
+            return (value.Item1, value.Item2);
+        }
+
+        public static implicit operator NewStruct((object, object) value)
+        {
+            return new NewStruct(value.Item1, value.Item2);
+        }
+    }
+}
---
+++ CSharpInstructor/Reflection/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* DortIslem dortIslem=new DortIslem(2,3);
+            Console.WriteLine(dortIslem.Topla(3, 4));
+            Console.WriteLine(dortIslem.Topla2());*/
+
+            var tip=typeof(DortIslem);
+            //var dortIslem =Activator.CreateInstance(tip);//Runtime da bunu olusturuyorsun
+            //DortIslem dortIslem = (DortIslem)Activator.CreateInstance(tip);//Runtime da bunu olusturuyorsun parametresiz Const kullaniyor
+            //DortIslem dortIslem = (DortIslem)Activator.CreateInstance(tip,6,7);
+
+            //Console.WriteLine(dortIslem.Topla(4, 5));
+            //Console.WriteLine(dortIslem.Topla2());
+
+           var instance=Activator.CreateInstance(tip, 6, 6);
+
+            MethodInfo methodInfo = instance.GetType().GetMethod("Topla2");
+
+            Console.WriteLine(methodInfo.Invoke(instance, null));
+
+            var metodlar = tip.GetMethods();
+
+            foreach (var info in metodlar) {
+                Console.WriteLine("Method Adi : {0}",info.Name);
+                foreach (var parametreInfo in info.GetParameters()) {
+                Console.WriteLine("Parametre Adi : {0}",parametreInfo.Name);
+                }
+                    }
+
+        }
+    }
+
+    public class DortIslem {
+
+        private int _sayi1;
+        private int _sayi2;
+
+        public DortIslem(int sayi1,int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2; 
+
+        }
+        public DortIslem()
+        {
+            
+        }
+
+        public int Topla(int sayi1,int sayi2) {
+
+            return sayi1 + sayi2;
+        
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+
+            return sayi1 * sayi2;
+
+        }
+
+
+        public int Topla2()
+        {
+
+            return _sayi1 + _sayi2;
+
+        }
+
+        public int Carp2()
+        {
+
+            return _sayi1 * _sayi2;
+
+        }
+
+
+
+    }
+
+}
